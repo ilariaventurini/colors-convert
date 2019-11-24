36237c08cd0599e8de4beee2ff337bed615cd05c
Ilaria Venturini <venturini.ila@gmail.com>
2019-11-24 11:05:31 +0100

    🚜 hex2rgb mini refactor

--- src/lib/color-utils.ts
+++ src/lib/color-utils.ts
@@ -27,12 +27,12 @@ export const color2cssString = (color: COLOR): string | undefined => {
 
 export const hex2rgb = (hex: HEX): RGB | undefined => {
   const RGB_HEX = /^#?(?:([0-9a-f]{3})[0-9a-f]?|([0-9a-f]{6})(?:[0-9a-f]{2})?)$/i
-  const [, short, long] = hex.match(RGB_HEX) || []
+  // short and long are or undefined or the original_hex without #
+  const [original_hex, short, long] = hex.match(RGB_HEX) || []
   if (long) {
     const value = Number.parseInt(long, 16)
     return { r: value >> 16, g: (value >> 8) & 0xff, b: value & 0xff }
   } else {
-    // short
     const rgbArray = Array.from(short, s => Number.parseInt(s, 16)).map(n => (n << 4) | n)
     return { r: rgbArray[0], g: rgbArray[1], b: rgbArray[2] }
   }
