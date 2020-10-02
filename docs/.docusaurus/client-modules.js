9eaa25fdcaf9a9b1d3494755fc14f2d7c2e331ee
Ilaria Venturini <venturini.ila@gmail.com>
2020-10-02 21:52:26 +0200

    ⚙️ Apply gitignore

--- docs/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/ilariaventurini/accurat/colors-convert/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/ilariaventurini/accurat/colors-convert/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/ilariaventurini/accurat/colors-convert/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/ilariaventurini/accurat/colors-convert/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/ilariaventurini/accurat/colors-convert/docs/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/ilariaventurini/accurat/colors-convert/docs/src/css/custom.css"),
-];
