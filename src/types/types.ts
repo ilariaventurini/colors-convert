01b438c4f529cfaca324f6dbc79c991eb07c1740
Ilaria Venturini <venturini.ila@gmail.com>
2020-10-18 15:54:30 +0200

    ✏️ Use interface instead of type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,8 @@
 export type HEX = string
-export type RGB = { r: number; g: number; b: number } // red green blue
-export type RGBA = { r: number; g: number; b: number; a: number } // red green blue alpha
-export type CMYK = { c: number; m: number; y: number; k: number } // cyan magenta yellow black
-export type HSL = { h: number; s: number; l: number } // hue saturation lightness
+export interface RGB { r: number; g: number; b: number } // red green blue
+export interface RGBA { r: number; g: number; b: number; a: number } // red green blue alpha
+export interface CMYK { c: number; m: number; y: number; k: number } // cyan magenta yellow black
+export interface HSL { h: number; s: number; l: number } // hue saturation lightness
 
 export type Color = HEX | RGB | RGBA | CMYK | HSL
 
