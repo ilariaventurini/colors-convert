da2dbadc586ea14aa88e7bece23249d550fdbd36
Ilaria Venturini <venturini.ila@gmail.com>
2020-10-24 15:01:19 +0200

    📝 mix: better doc

--- src/lib/mix.ts
+++ src/lib/mix.ts
@@ -3,13 +3,18 @@ import { color2rgb } from './rgb'
 import { sum, sumBy, round } from 'lodash'
 import { applyFnToEachObjValue } from './utils'
 
-// Mix two or more colors based on their weights.
-// Given [c1, c2, c3, ...] and [w1, w2, w3, ...], it returns:
-//   mixedCol = {
-//      r: sqrt(r1^2 * w1 + r2^2 * w2 + r3^2 * w3 + ...)
-//      g: sqrt(g1^2 * w1 + g2^2 * w2 + g3^2 * w3 + ...)
-//      b: sqrt(b1^2 * w1 + b2^2 * w2 + b3^2 * w3 + ...)
-//   }
+/**
+ * Mix two or more colors based on their weights.
+ * Given [c1, c2, c3, ...] and [w1, w2, w3, ...], it returns:
+ *   mixedCol = {
+ *      r: sqrt(r1^2 * w1 + r2^2 * w2 + r3^2 * w3 + ...)
+ *      g: sqrt(g1^2 * w1 + g2^2 * w2 + g3^2 * w3 + ...)
+ *      b: sqrt(b1^2 * w1 + b2^2 * w2 + b3^2 * w3 + ...)
+ *   }
+ * @param colors array of Colors
+ * @param weights array of weights
+ * @returns RGB object
+ */
 export function mix(colors: Color[], weights?: number[]): RGB {
   // defaulting weights
   const ws = getWeights(colors, weights)
@@ -18,9 +23,9 @@ export function mix(colors: Color[], weights?: number[]): RGB {
   const rgbs = colors.map(color2rgb)
 
   // create the mixed color
-  const weightedRgbs = rgbs.map((rgb, i) => {
-    return weightedRgb(rgb, ws[i])
-  })
+
+  // weighs each component (r, g, b) of each color
+  const weightedRgbs = rgbs.map((rgb, i) => weightedRgb(rgb, ws[i]))
 
   const weightedR = Math.sqrt(sumBy(weightedRgbs, 'cPwR'))
   const weightedG = Math.sqrt(sumBy(weightedRgbs, 'cPwG'))
@@ -30,7 +35,13 @@ export function mix(colors: Color[], weights?: number[]): RGB {
   return applyFnToEachObjValue(mix, (c: number) => round(c)) as RGB
 }
 
-const weightedRgb = (rgb: RGB, weight: number) => {
+/**
+ * Weigh each color component (r, g, b) of the color.
+ * @param rgb color to be weighted
+ * @param weight weight value
+ * @returns weighted rgb color
+ */
+function weightedRgb(rgb: RGB, weight: number) {
   const { r, g, b } = rgb
   const cPwR = r * r * weight
   const cPwG = g * g * weight
@@ -38,7 +49,14 @@ const weightedRgb = (rgb: RGB, weight: number) => {
   return { cPwR, cPwG, cPwB }
 }
 
-const getWeights = (colors: Color[], weights?: number[]): number[] => {
+/**
+ * If weights array is not undefined, checks that its ok (sum must be 1 and colors and weights must the of the same lenght) and returns it.
+ * If weight is undefined, fill it with the default values (1 / colors.lenght) and returns it.
+ * @param colors array of colors
+ * @param weights array of weights
+ * @returns array of weights.
+ */
+function getWeights(colors: Color[], weights?: number[]): number[] {
   const defaultWeight = 1 / colors.length
   const defaultWeights = Array(colors.length).fill(defaultWeight)
   if (weights && checkWeights(colors, weights)) {
@@ -48,7 +66,13 @@ const getWeights = (colors: Color[], weights?: number[]): number[] => {
   }
 }
 
-const checkWeights = (colors: Color[], weights: number[]) => {
+/**
+ * Check that the sum of weights is 1 and that colors and weights have the same lenght.
+ * @param colors array of colors
+ * @param weights array of weights
+ * @returns true if the sum of weights is 1 and if colors and weights have the same lenght, false otherwise
+ */
+function checkWeights(colors: Color[], weights: number[]) {
   const tot = sum(weights)
   if (tot !== 1) {
     throw new Error(`The sum of the weights should be 1, instead is ${tot}.`)
